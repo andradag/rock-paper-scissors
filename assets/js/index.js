4ac4a98083ff290115c6d98bdd0c4e7fb1fe67ab
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-02-25 11:53:24 +0000

    fix update score and display score when player choice is undefined

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,12 +13,16 @@ const getUserChoice = function () {
     "Please select Rock (R), Paper (P) or Scissors (S)"
   );
 
-  // validate the choice
-  // if not valid then present and alert
-  if (userChoice === "R" || userChoice === "P" || userChoice === "S") {
-    return userChoice;
+  if (!userChoice) {
+    isGameInProgress = false;
   } else {
-    alert("Please select one of the following: R, P or S");
+    // validate the choice
+    // if not valid then present and alert
+    if (userChoice === "R" || userChoice === "P" || userChoice === "S") {
+      return userChoice;
+    } else {
+      alert("Please select one of the following: R, P or S");
+    }
   }
 };
 
@@ -78,7 +82,7 @@ const updateScore = function (gameOutcome) {
 
 // display the score of player in the console logs
 const displayScore = function (gameOutcome) {
-  console.log("Game Outcome: " + gameOutcome);
+  console.log("Game Outcome for player 1: " + gameOutcome);
   // console log the score
   console.log(
     "Score: W - " +
@@ -93,21 +97,22 @@ const displayScore = function (gameOutcome) {
 const playGame = function () {
   // get the player 1 choice
   const player1Choice = getUserChoice();
-  console.log("player1Choice", player1Choice);
+  console.log("Player 1 plays: " + player1Choice);
 
   // get the computer choice
   const computerChoice = getComputerChoice();
-  console.log("computerChoice", computerChoice);
+  console.log("Computer plays: " + computerChoice);
 
-  // feed the player choice and computer choice to game logic
-  const gameOutcome = gameLogic(player1Choice, computerChoice);
-  console.log("gameOutcome", gameOutcome);
+  if (player1Choice && computerChoice) {
+    // feed the player choice and computer choice to game logic
+    const gameOutcome = gameLogic(player1Choice, computerChoice);
 
-  // update the scores
-  updateScore(gameOutcome);
+    // update the scores
+    updateScore(gameOutcome);
 
-  // display the scores
-  displayScore(gameOutcome);
+    // display the scores
+    displayScore(gameOutcome);
+  }
 };
 
 // present the user with a confirm model yes/no
@@ -123,11 +128,13 @@ while (isGameInProgress) {
   // start the game by calling this function
   playGame();
 
-  // ask the user if they want to continue
-  const isPlayAgain = playAgain();
+  if (isGameInProgress) {
+    // ask the user if they want to continue
+    const isPlayAgain = playAgain();
 
-  // is playAgain is false set isGameInProgress to false
-  if (!isPlayAgain) {
-    isGameInProgress = false;
+    // is playAgain is false set isGameInProgress to false
+    if (!isPlayAgain) {
+      isGameInProgress = false;
+    }
   }
 }
